ffccc4b15a253016d0cfbdfa63d1f9871c25092f
Aaron Mathews <aamathews23@gmail.com>
2022-09-15 20:29:45 -0700

    added operate func to main method

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,17 @@ fn main() {
   let mut args: Args = args();
 
   let first = args.nth(1).unwrap();
-  let operator = args.nth(0).unwrap();
+  let operator = args.nth(0).unwrap().chars().next().unwrap();
   let second = args.nth(0).unwrap();
 
   let first_num = first.parse::<f32>().unwrap();
   let second_num = second.parse::<f32>().unwrap();
+  let result = operate(operator, first_num, second_num);
 
-  println!("{:?} {} {}", first, operator, second);
+  println!("{:?}", result);
 }
 
-fn operator(operator: char, first_num: f32, second_num: f32) -> f32 {
+fn operate(operator: char, first_num: f32, second_num: f32) -> f32 {
   if operator == '+' {
     first_num + second_num
   } else if operator == '-' {
