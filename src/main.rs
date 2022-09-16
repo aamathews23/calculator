1605db7fb973fd0c95b26e6f571ce6966f7fe6b6
Aaron Mathews <aamathews23@gmail.com>
2022-09-15 20:23:48 -0700

    parse strings to nums

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,9 @@ fn main() {
   let first = args.nth(1).unwrap();
   let operator = args.nth(0).unwrap();
   let second = args.nth(0).unwrap();
+
+  let first_num = first.parse::<f32>().unwrap();
+  let second_num = second.parse::<f32>().unwrap();
+
   println!("{:?} {} {}", first, operator, second);
 }
